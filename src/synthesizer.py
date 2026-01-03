33443ebf19af43fa42dc43052ebf25c880627dca
Kartik Mehta <kartikmehta0802@gmail.com>
2026-01-02 17:54:54 -0800

    generate function hopefully works

--- src/synthesizer.py
+++ src/synthesizer.py
@@ -1,6 +1,7 @@
 import torch 
 import torch.nn as nn
 import torch.nn.functional as F
+from speaker_encoder import SpeakerEncoder
 
 
 class Encoder(nn.Module):
@@ -34,16 +35,17 @@ class Encoder(nn.Module):
         self.bi_lstm = nn.LSTM(lstm_in, hidden_channels, lstm_layers, batch_first=batch_first, bidirectional=bidirectional)
 
     def forward(self, x):
-        x = self.emb(x)
+        x = self.emb(x).transpose(1, 2) # (B, n_emb, T)
+        x = x[:, None, :, :] # (B, 1, n_emb, T)
 
         x = self.relu(self.bn1(self.conv1(x)))
         x = self.relu(self.bn2(self.conv2(x)))
         x = self.relu(self.bn3(self.conv3(x)))
 
-        B, C, T, D = x.shape
-        x = x.transpose(1, 2).contiguous().view(B, T, C*D)
+        B, C, D, T = x.shape
+        x = x.transpose(1, 3).contiguous().view(B, T, C*D)
 
-        x = self.bi_lstm(x)
+        x, _ = self.bi_lstm(x)
 
         return x # (B, T, hidden_channels*2)
 
@@ -86,11 +88,12 @@ class TemporalAttention(nn.Module):
         kv = self.kv(kvproj) # (B, T, attn_dim*2)
         k, v = kv.split(self.attn_dim, dim=-1) # (B, T, attn_dim)
 
-        energy = (q @ k.transpose(-2, -1)) + loc_feats + cum_feats # (B, T)
+        energy = torch.einsum("bd,btd->bt", q, k) # (B, T)
+        energy = energy + loc_feats + cum_feats # (B, T)
 
         a_t = F.softmax(energy, dim=-1)
 
-        context = a_t @ v # (B, attn_dim)
+        context = torch.einsum("bt,btd->bd", a_t, v) # (B, attn_dim)
 
         return context, a_t, energy
 
@@ -169,6 +172,7 @@ class Decoder(nn.Module):
         super().__init__()
         self.lstm_layers = lstm_layers
         self.lstm_dim = lstm_dim
+        self.mel_dim = mel_dim
 
         self.pre_net = PreNet(pre_in, pre_out)
 
@@ -193,10 +197,14 @@ class Decoder(nn.Module):
             mel_dim
         )
 
+        self.gate_proj = nn.Linear(lstm_dim, 1)
+
     def forward(self, x, mel_seq):
         device = x.device
         B, t_enc, _ = x.shape
         mel_out = torch.zeros_like(mel_seq, device=device)
+
+        gate_out = []
 
         for t in range(mel_seq.size(1)):
             if t == 0:
@@ -222,7 +230,12 @@ class Decoder(nn.Module):
             out, (h, c) = self.lstm(lstm_in, (h, c)) # out: (B, 1, lstm_dim)
             s_t = out[:, 0] # (B, lstm_dim)
             mel_frame = self.mel_proj(s_t) # (B, mel_dim)
+            gate_t = self.gate_proj(s_t).squeeze(-1)
+
             mel_out[:, t] = mel_frame
+            gate_out.append(gate_t.unsqueeze(1))
+        
+        gate_out = torch.cat(gate_out, dim=1)
         
         mel_4D = mel_out[:, None, :, :].transpose(-2, -1) # (B, 1, mel_dim, T)
         mel_post_4d = self.post_net(mel_4D) # (B, 1, mel_dim, T)
@@ -230,69 +243,56 @@ class Decoder(nn.Module):
 
         mel_out_post = mel_out + mel_post
 
+        return mel_out, mel_out_post, gate_out
+
+    def generate(self, max_new_frames, voice_embedding, synth_encoder, text_ids, device=None):
+        h_enc = synth_encoder(text_ids).to(device)
+        h_enc = torch.cat([h_enc, voice_embedding], dim=-1) # (B, T, enc_dim + voice_dim)
+
+        B, t_enc, _ = h_enc.shape
+
+        h = torch.zeros(self.lstm_layers, B, self.lstm_dim, device=device)
+        c = torch.zeros(self.lstm_layers, B, self.lstm_dim, device=device)
+        s_t = h[-1]
+
+        prev_attn = torch.ones(B, t_enc, device=device)
+        prev_attn = F.normalize(prev_attn, p=1, dim=-1)
+        prev_energy = torch.zeros(B, t_enc, device=device)
+
+        prev_mel = torch.zeros(B, self.mel_dim, device=device)
+
+        mel_frames = []
+
+        for _ in range(max_new_frames):
+            context, a_t, energy = self.temp_attn(s_t, h_enc, prev_attn, prev_energy)
+            prev_attn, prev_energy = a_t, energy
+
+            pre_out = self.pre_net(prev_mel) # (B, pre_out)
+            lstm_in = torch.cat([context, pre_out], dim=-1) # (B, attn_dim + pre_out)
+            lstm_in = lstm_in.unsqueeze(1)  
+
+            out, (h, c) = self.lstm(lstm_in, (h, c)) # out: (B, 1, lstm_dim)
+            s_t = out[:, 0] # (B, lstm_dim)
+
+            mel_t = self.mel_proj(s_t) # (B, mel_dim)
+            mel_frames.append(mel_t.unsqueeze(1))
+
+            prev_mel = mel_t
+            
+            stop_token = self.gate_proj(s_t).squeeze(-1) # (B,)
+            stop_prob = torch.sigmoid(stop_token)
+
+            gate_threshold = 0.5
+            if (stop_prob > gate_threshold).all():
+                break
+
+        mel_out = torch.cat(mel_frames, dim=1) # (B, T_dec, mel_dim)
+
+        mel_4D = mel_out.transpose(1, 2).unsqueeze(1) # (B, 1, mel_dim, T_dec)
+        mel_post_4d = self.post_net(mel_4D) # (B, 1, mel_dim, T_dec)
+        mel_post = mel_post_4d.squeeze(1).transpose(1, 2) # (B, T_dec, mel_dim)
+
+        mel_out_post = mel_out + mel_post
+
         return mel_out, mel_out_post
-
-
-class Synthesizer(nn.Module):
-    def __init__(
-            self,
-            pre_in,
-            pre_out,
-            eh_state,
-            attn_dim,
-            loc_kernel_size,
-            loc_padding,
-            pos_kernel_size,
-            pos_padding,
-            lstm_dim,
-            lstm_layers,
-            mel_dim,
-            post_in,
-            post_filters,
-            post_kernel,
-            post_padding,
-            vocab_size,
-            enc_emb,
-            enc_channels,
-            enc_filters,
-            enc_kernel_size, 
-            enc_hidden_channels,
-            enc_lstm_layers,
-            enc_lstm_in,
-            batch_first=True,
-            enc_bidirectional=False,
-            dec_bidirectional=False
-    ):
-        self.encoder = Encoder(
-            vocab_size,
-            enc_emb,
-            enc_channels,
-            enc_filters,
-            enc_kernel_size, 
-            enc_hidden_channels,
-            enc_lstm_layers,
-            enc_lstm_in,
-            batch_first=batch_first
-            bidirectional=enc_bidirectional
-        )
-
-        self.decoder = Decoder(
-            pre_in,
-            pre_out,
-            eh_state,
-            attn_dim,
-            loc_kernel_size,
-            loc_padding,
-            pos_kernel_size,
-            pos_padding,
-            lstm_dim,
-            lstm_layers,
-            mel_dim,
-            post_in,
-            post_filters,
-            post_kernel,
-            post_padding,
-            batch_first=batch_first,
-            dec_bidirectional=dec_bidirectional
-        )
 
